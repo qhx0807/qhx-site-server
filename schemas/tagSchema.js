b4177f7891547dec68d09f61d0641e2f038e720b
qhx0807 <820240134@qq.com>
2017-11-20 23:28:26 +0800

    add insert tag :whale:

--- schemas/tagSchema.js
+++ schemas/tagSchema.js
@@ -8,6 +8,9 @@ TagSchema.statics = {
   query: function(cb) {
     return this.find().exec(cb)
   },
+  insert: function(doc, cb) {
+    return this.create(doc).exec(cb)
+  },
   update: function(id, doc, cb){
     return this.update({_id: id}, doc).exec(cb)
   },
