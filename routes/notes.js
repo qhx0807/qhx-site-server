eb2109bafbb9a2f6ae6628738129756e46c3354c
qhx0807 <820240134@qq.com>
2017-11-21 22:39:54 +0800

    update:whale:

--- routes/notes.js
+++ routes/notes.js
@@ -7,7 +7,7 @@ var Notes = require('../models/notesModel')
 
 //查询所有数据
 router.get('/', function(req, res, next) {
-    Notes.fetch(function(err, notes) {
+    Notes.query(function(err, notes) {
     if (err) {
       console.log(err)
     }
