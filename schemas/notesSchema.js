8ec3c56500822c58247593faaa9152c6d348f6a2
qhx0807 <820240134@qq.com>
2017-11-21 22:40:21 +0800

    update :whale:

--- schemas/notesSchema.js
+++ schemas/notesSchema.js
@@ -5,8 +5,17 @@ var NotesSchema = new mongoose.Schema({
 })
 
 NotesSchema.statics = {
-  fetch: function(cb) {
+  query: function(cb) {
     return this.find().exec(cb)
-  }
+  },
+  insert: function(doc, cb) {
+    return this.create(doc).exec(cb)
+  },
+  update: function(id, doc, cb){
+    return this.update({_id: id}, doc).exec(cb)
+  },
+  delete: function(id, cb){
+    return this.remove({_id: id}).exec(cb)
+  },
 }
 module.exports = NotesSchema
