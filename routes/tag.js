cd8b86349c6f7b04368a0a814f866a0edf397665
qhx0807 <820240134@qq.com>
2017-11-22 15:02:01 +0800

    :rocket: tag CRUD

--- routes/tag.js
+++ routes/tag.js
@@ -2,15 +2,43 @@ var express = require('express')
 var router = express.Router()
 var TagModel = require('../service/tag-service')
 
-
 router.get('/tags', function(req, res, next) {
   TagModel.query(function(err, tags) {
     if (err) {
       console.log(err)
     }
-    res.json({Data: tags})
+    res.json({ Data: tags })
   })
 })
 
+router.post('/tags', function(req, res, next) {
+  var TagInsert = new TagModel(req.body)
+  TagInsert.save(function(err) {
+    if (err) {
+      res.json({ Data: err })
+    } else {
+      res.json({ OK: 'ok' })
+    }
+  })
+})
+
+router.put('/tags', function(req, res, next) {
+  TagModel.edit(req.body.id, req.body, function(err, docs) {
+    if (err) {
+      res.json({ Data: err })
+    }
+    res.json({ Data: docs })
+  })
+})
+
+router.delete('/tags', function(req, res, next) {
+  TagModel.delete(req.body.id, function(err, docs) {
+    if (err) {
+      res.json({ Data: err })
+    } else {
+      res.json({ OK: 'ok' })
+    }
+  })
+})
 
 module.exports = router
--- service/tag-service.js
+++ service/tag-service.js
@@ -1,19 +1,21 @@
 var mongoose = require('mongoose')
 
 var TagSchema = new mongoose.Schema({
-  title: String
+  name: String,
+  url: String,
+  memo: String
 })
 
 TagSchema.statics = {
   query: function(cb) {
     return this.find().exec(cb)
   },
-  update: function(id, doc, cb){
-    return this.update({_id: id}, doc).exec(cb)
+  edit: function(id, doc, cb) {
+    return this.update({ _id: id }, doc).exec(cb)
   },
-  delete: function(id, cb){
-    return this.remove({_id: id}).exec(cb)
-  },
+  delete: function(id, cb) {
+    return this.remove({ _id: id }).exec(cb)
+  }
 }
 
 var TagModel = mongoose.model('tag', TagSchema)
