c031cd700598c1ad445216f9ea766a6276218abe
qhx0807 <820240134@qq.com>
2017-12-05 23:35:11 +0800

    :construction: update sta query

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,13 +1,16 @@
 var express = require('express')
 var router = express.Router()
 var PvModel = require('../service/pv-service')
+var LogModel = require('../service/log-service')
 
-router.get('/pv', function (req, res, next) {
-  PvModel.query(req.query.date, function (err, message) {
-    if (err) {
-      console.log(err)
-    }
-    res.json({ Data: message })
+router.get('/sta', function (req, res, next) {
+  var obj = {}
+  PvModel.fetch().then(function (pv) {
+    obj.pv = pv.length
+    return LogModel.fetch()
+  }).then(function (log) {
+    obj.api = log.length
+    res.json(obj)
   })
 })
 
